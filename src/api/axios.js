ab628f4edff42f7beb689c5819e28b8288f03a97
Kasper4649 <imzxz123456@gmail.com>
2020-05-29 20:11:15 +0800

    [feature]新增等待请求加载功能，优化用户体验

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Loading } from 'element-ui';
 
 const request = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
@@ -7,5 +8,35 @@ const request = axios.create({
     'Content-Type': 'multipart/form-data',
   },
 });
+
+let loading;
+
+function startLoading() {
+  loading = Loading.service({
+    lock: true,
+    text: 'Loading',
+    fullscreen: true,
+    background: 'rgba(0, 0, 0, 0.3)'
+  })
+}
+
+function endLoading() {
+  loading.close();
+}
+
+request.interceptors.request.use(config => {
+  startLoading();
+  return config;
+}, error => {
+  return Promise.reject(error);
+})
+
+request.interceptors.response.use(response => {
+  endLoading();
+  return response;
+}, error => {
+  endLoading();
+  return Promise.reject(error);
+})
 
 export default request;
--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import { Loading } from 'element-ui';
+import './assets/element.scss';
 
 Vue.use(VueAxios, axios);
+Vue.use(Loading);
 
 Vue.config.productionTip = false;
 
